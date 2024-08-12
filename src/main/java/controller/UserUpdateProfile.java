717404326cfcb38d7054ff7800b223ae1006a184
bikash2060 <bkashbhattarai@gmail.com>
2024-08-12 19:17:36 +0545

    Required validation during profile update applied

--- src/main/java/controller/UserUpdateProfile.java
+++ src/main/java/controller/UserUpdateProfile.java
@@ -5,26 +5,49 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import model.User;
+import service.UserDao;
 import utils.SignUpValidation;
 import utils.StringUtils;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.SQLException;
 
 @WebServlet(asyncSupported = true, urlPatterns = StringUtils.USER_PROFILE_UPDATE_SERVLET)
 public class UserUpdateProfile extends HttpServlet {
     private static final long serialVersionUID = 1L;
+    private UserDao userDao;
+
+    public void init() throws ServletException {
+        super.init();
+        try {
+            this.userDao = new UserDao();
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         String userID = req.getParameter("userID");
+        int userId = Integer.parseInt(userID);
         String firstName = req.getParameter("firstName");
         String lastName = req.getParameter("lastName");
         String emailAddress = req.getParameter("emailAddress");
         String phoneNumber = req.getParameter("phoneNumber");
         String username = req.getParameter("username");
+
+        // Validation for Empty Field
+        if((firstName == null || firstName.isEmpty())|| (lastName == null || lastName.isEmpty()) || (phoneNumber == null || phoneNumber.isEmpty()) ||
+            (emailAddress == null || emailAddress.isEmpty()) || (username == null || username.isEmpty())) {
+            req.setAttribute(StringUtils.ERROR_MESSAGE_KEY, StringUtils.EMPTY_FIELD_ERROR_MESSAGE);
+            req.getRequestDispatcher(StringUtils.USER_PROFILE_PAGE).forward(req, resp);
+            return;
+        }
 
         // Validation Check for First Name
         if(!SignUpValidation.isValidFirstName(firstName)){
@@ -55,11 +78,44 @@ public class UserUpdateProfile extends HttpServlet {
         }
 
         User newDetails = new User();
+        newDetails.setUserID(userId);
         newDetails.setFirstName(firstName);
         newDetails.setLastName(lastName);
         newDetails.setEmailAddress(emailAddress);
         newDetails.setPhoneNumber(phoneNumber);
         newDetails.setUsername(username);
 
+        int result = userDao.updateUser(newDetails);
+        switch (result){
+            case 1:
+                req.getSession().setAttribute(StringUtils.SUCCESS_MESSAGE_KEY, StringUtils.USER_UPDATE_SUCCESS_MESSAGE);
+                resp.sendRedirect(req.getContextPath() + StringUtils.USER_PROFILE_DETAILS_SERVLET);
+                break;
+
+            case 0:
+                req.setAttribute(StringUtils.ERROR_MESSAGE_KEY, StringUtils.USER_UPDATE_ERROR_MESSAGE);
+                req.getRequestDispatcher(StringUtils.USER_PROFILE_PAGE).forward(req, resp);
+                break;
+
+            case -1:
+                req.setAttribute(StringUtils.ERROR_MESSAGE_KEY, StringUtils.DUPLICATE_EMAIL_ERROR_MESSAGE);
+                req.getRequestDispatcher(StringUtils.USER_PROFILE_PAGE).forward(req, resp);
+                break;
+
+            case -2:
+                req.setAttribute(StringUtils.ERROR_MESSAGE_KEY, StringUtils.DUPLICATE_PHONE_NUMBER_ERROR_MESSAGE);
+                req.getRequestDispatcher(StringUtils.USER_PROFILE_PAGE).forward(req, resp);
+                break;
+
+            case -3:
+                req.setAttribute(StringUtils.ERROR_MESSAGE_KEY, StringUtils.DUPLICATE_USERNAME_ERROR_MESSAGE);
+                req.getRequestDispatcher(StringUtils.USER_PROFILE_PAGE).forward(req, resp);
+                break;
+
+            case -4:
+                req.setAttribute(StringUtils.ERROR_MESSAGE_KEY, StringUtils.SERVER_ERROR_MESSAGE);
+                req.getRequestDispatcher(StringUtils.USER_PROFILE_PAGE).forward(req, resp);
+                break;
+        }
     }
 }
