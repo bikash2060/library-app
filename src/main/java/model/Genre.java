10ab756a1b3e8d51afb383be04771c409bac2ef0
bikash2060 <bkashbhattarai@gmail.com>
2024-09-04 19:15:57 +0545

    Genre model class created

--- src/main/java/model/Genre.java
+++ src/main/java/model/Genre.java
@@ -1,2 +1,30 @@
-package model;public class Genre {
+package model;
+
+public class Genre {
+
+    private int genreID;
+    private String genreName;
+
+    public Genre() {}
+
+    public Genre(int genreID, String genreName) {
+        this.genreID = genreID;
+        this.genreName = genreName;
+    }
+
+    public int getGenreID() {
+        return genreID;
+    }
+
+    public void setGenreID(int genreID) {
+        this.genreID = genreID;
+    }
+
+    public String getGenreName() {
+        return genreName;
+    }
+
+    public void setGenreName(String genreName) {
+        this.genreName = genreName;
+    }
 }
