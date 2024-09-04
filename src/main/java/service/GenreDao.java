1fd5e838ee247c49e0d73854228d7ce1b039d8aa
bikash2060 <bkashbhattarai@gmail.com>
2024-09-04 19:16:20 +0545

    Service class for Genre model created

--- src/main/java/service/GenreDao.java
+++ src/main/java/service/GenreDao.java
@@ -1,2 +1,43 @@
-package service;public class GenreDao {
+package service;
+
+import model.Genre;
+import utils.DatabaseConnection;
+import utils.StringUtils;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenreDao {
+
+    Connection con;
+
+    public GenreDao() {
+        try{
+            this.con = DatabaseConnection.getDatabaseConnection();
+        }
+        catch(SQLException | ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+
+    public List<Genre> getAllGenre(){
+        List<Genre> genres = new ArrayList<>();
+        try{
+            PreparedStatement ps = con.prepareStatement(StringUtils.GET_ALL_GENRE);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Genre newGenre = new Genre();
+                newGenre.setGenreID(rs.getInt("GenreID"));
+                newGenre.setGenreName(rs.getString("GenreName"));
+                genres.add(newGenre);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return genres;
+    }
 }
